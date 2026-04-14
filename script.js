56ac63ec9b20990ed7b118d9a577981c20d445ff
VitorHugoVaccari <luvaccari88@gmail.com>
2026-04-14 15:30:15 -0300

    refactor: improve reset logic and code structure in etch-a-sketch

--- script.js
+++ script.js
@@ -15,26 +15,40 @@ button.addEventListener("click", function () {
   } else if (qtd.value <= 0) {
     qtd.value = 1;
   }
-  grade_pct = 100 / qtd.value;
   container.innerHTML = "";
   for (let i = 0; i < qtd.value; i++) {
     const novaDiv = document.createElement("div");
     novaDiv.className = "grade";
     let enterCount = 0;
+    let opacity = 0;
     novaDiv.innerHTML = `<p>${enterCount}</p>`;
 
     reset.addEventListener("click", function () {
+      enterCount = 0;
+      opacity = 0;
       novaDiv.style.backgroundColor = "";
+      novaDiv.innerHTML = `<p>${enterCount}</p>`;
     });
+
+    let r;
+    let g;
+    let b;
+
     novaDiv.addEventListener("mouseenter", function () {
       if (enterCount === 0) {
-        const rnd1 = randomNum(1, 255);
-        const rnd2 = randomNum(1, 255);
-        const rnd3 = randomNum(1, 255);
+        r = randomNum(1, 255);
+        g = randomNum(1, 255);
+        b = randomNum(1, 255);
 
-        novaDiv.style.backgroundColor = `rgba(${rnd1}, ${rnd2}, ${rnd3})`;
+        novaDiv.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${opacity})`;
       }
-      enterCount += 1;
+
+      if (enterCount <= 9) {
+        enterCount += 1;
+        opacity += 0.1;
+        novaDiv.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${opacity})`;
+      }
+
       novaDiv.innerHTML = `<p>${enterCount}</p>`;
     });
 
